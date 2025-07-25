4942045bfc240c21f1d744674965438580880285
masmahmoodi <mrm7g8@gmail.com>
2025-07-25 11:47:31 -0700

    changed the structure of project

--- src/Pages/Host/HostVanInfo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function HostVanInfo() {
-    return (
-        <h2>Detailed info goes here</h2>
-    )
-}
--- src/Pages/Host/HostVanPhotos.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function HostVanPhotos() {
-    return (
-        <h2>Photos go here</h2>
-    )
-}
